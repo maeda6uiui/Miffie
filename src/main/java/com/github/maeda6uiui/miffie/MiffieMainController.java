cb3927e7a1b7582308fa251db76f748ebda763cd
maeda6uiui <63230315+maeda6uiui@users.noreply.github.com>
2023-10-27 15:11:11 +0900

    Add controller

---
+++ src/main/java/com/github/maeda6uiui/miffie/MiffieMainController.java
@@ -0,0 +1,20 @@
+package com.github.maeda6uiui.miffie;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.MenuItem;
+
+/**
+ * Controller for the GUI components
+ *
+ * @author maeda6uiui
+ */
+public class MiffieMainController {
+    @FXML
+    private MenuItem miNew;
+
+    @FXML
+    protected void onActionMiNew(ActionEvent event) {
+
+    }
+}
