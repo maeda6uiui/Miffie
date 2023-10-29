ef0ff1f4ef3a101b42726abc8311801a74874c45
maeda6uiui <63230315+maeda6uiui@users.noreply.github.com>
2023-10-29 09:12:16 +0900

    Use requireNonNull

--- src/main/java/com/github/maeda6uiui/miffie/MiffieApp.java
+++ src/main/java/com/github/maeda6uiui/miffie/MiffieApp.java
@@ -7,6 +7,7 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Objects;
 
 /**
  * Entry point for the Miffie app
@@ -16,7 +17,8 @@ import java.io.IOException;
 public class MiffieApp extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        Parent root = FXMLLoader.load(this.getClass().getResource("miffie_main_view.fxml"));
+        Parent root = FXMLLoader.load(
+                Objects.requireNonNull(this.getClass().getResource("miffie_main_view.fxml")));
         var scene = new Scene(root, 1000, 400);
 
         stage.setTitle("Miffie - MIF Editor");
