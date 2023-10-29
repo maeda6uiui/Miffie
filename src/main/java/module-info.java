549cc12a4be2d8e3c8855f5a16a554e12414aa4f
maeda6uiui <63230315+maeda6uiui@users.noreply.github.com>
2023-10-29 09:48:54 +0900

    Add dependencies

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,10 @@
 module com.github.maeda6uiui.miffie {
     requires javafx.controls;
     requires javafx.fxml;
+    requires com.fasterxml.jackson.core;
+    requires com.fasterxml.jackson.databind;
+    requires com.fasterxml.jackson.dataformat.yaml;
+    requires com.github.dabasan.jxm.mif;
 
     opens com.github.maeda6uiui.miffie to javafx.fxml;
     exports com.github.maeda6uiui.miffie;
