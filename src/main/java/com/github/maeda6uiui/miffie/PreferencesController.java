039d7bb5197913b1d2c37a4be9087206bf6536a5
maeda6uiui <63230315+maeda6uiui@users.noreply.github.com>
2023-11-02 12:33:00 +0900

    Add PreferencesViewModel

--- src/main/java/com/github/maeda6uiui/miffie/PreferencesController.java
+++ src/main/java/com/github/maeda6uiui/miffie/PreferencesController.java
@@ -1,9 +1,14 @@
 package com.github.maeda6uiui.miffie;
 
+import javafx.beans.binding.Bindings;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
+import javafx.stage.Stage;
+import javafx.util.converter.NumberStringConverter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -14,6 +19,8 @@ import java.util.ResourceBundle;
  * @author maeda6uiui
  */
 public class PreferencesController implements Initializable {
+    private static final Logger logger = LoggerFactory.getLogger(PreferencesController.class);
+
     @FXML
     private Button btnCancel;
     @FXML
@@ -101,9 +108,59 @@ public class PreferencesController implements Initializable {
     @FXML
     private TextField tfWWindowWidth;
 
+    private PreferencesViewModel viewModel;
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        viewModel = new PreferencesViewModel();
+        viewModel.lDisplayLanguageProperty().bindBidirectional(cbLDisplayLanguage.selectionModelProperty());
+        viewModel.tThemeProperty().bindBidirectional(cbTTheme.selectionModelProperty());
+        viewModel.ivDarkScreenProperty().bindBidirectional(ckbIVDarkScreen.selectedProperty());
+        viewModel.ivExtraHitcheckProperty().bindBidirectional(ckbIVExtraHitcheck.selectedProperty());
+        viewModel.ivMissionBriefingProperty().bindBidirectional(taIVMissionBriefing.textProperty());
+        viewModel.ivArticleDefinitionFilepathProperty().bindBidirectional(tfIVArticleDefinitionFilepath.textProperty());
+        viewModel.ivBD1FilepathProperty().bindBidirectional(tfIVBD1Filepath.textProperty());
+        viewModel.ivImage1FilepathProperty().bindBidirectional(tfIVImage1Filepath.textProperty());
+        viewModel.ivImage2FilepathProperty().bindBidirectional(tfIVImage2Filepath.textProperty());
+        viewModel.ivMissionLongNameProperty().bindBidirectional(tfIVMissionLongName.textProperty());
+        viewModel.ivMissionShortNameProperty().bindBidirectional(tfIVMissionShortName.textProperty());
+        viewModel.ivPD1FilepathProperty().bindBidirectional(tfIVPD1Filepath.textProperty());
+        Bindings.bindBidirectional(
+                tfIVSkyType.textProperty(),
+                viewModel.ivSkyTypeProperty(),
+                new NumberStringConverter()
+        );
+        Bindings.bindBidirectional(
+                tfMMaxNumHalfWidthCharactersInLine.textProperty(),
+                viewModel.mMaxNumHalfWidthCharactersInLineProperty(),
+                new NumberStringConverter()
+        );
+        Bindings.bindBidirectional(
+                tfMMaxNumLines.textProperty(),
+                viewModel.mMaxNumLinesProperty(),
+                new NumberStringConverter()
+        );
+        viewModel.mReadEncodingProperty().bindBidirectional(tfMReadEncoding.textProperty());
+        viewModel.mWriteEncodingProperty().bindBidirectional(tfMWriteEncoding.textProperty());
+        viewModel.tCustomThemeFilepathProperty().bindBidirectional(tfTCustomThemeFilepath.textProperty());
+        Bindings.bindBidirectional(
+                tfWWindowHeight.textProperty(),
+                viewModel.wWindowHeightProperty(),
+                new NumberStringConverter()
+        );
+        Bindings.bindBidirectional(
+                tfWWindowWidth.textProperty(),
+                viewModel.wWindowWidthProperty(),
+                new NumberStringConverter()
+        );
 
+        boolean b = viewModel.populate(cbLDisplayLanguage, cbTTheme);
+        if (!b) {
+            logger.error("Failed to initialize the preferences view");
+
+            Stage stage = (Stage) lblLDisplayLanguage.getScene().getWindow();
+            stage.close();
+        }
     }
 
     @FXML
---
+++ src/main/java/com/github/maeda6uiui/miffie/PreferencesViewModel.java
@@ -0,0 +1,378 @@
+package com.github.maeda6uiui.miffie;
+
+import javafx.beans.property.*;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
+import javafx.scene.control.SingleSelectionModel;
+import javafx.util.Callback;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.nio.charset.Charset;
+import java.util.List;
+
+/**
+ * View model for the preferences view
+ *
+ * @author maeda6uiui
+ */
+public class PreferencesViewModel {
+    private static final Logger logger = LoggerFactory.getLogger(PreferencesViewModel.class);
+
+    private ObjectProperty<SingleSelectionModel<DisplayLanguage>> lDisplayLanguage;
+    private ObjectProperty<SingleSelectionModel<MiffieTheme>> tTheme;
+    private BooleanProperty ivDarkScreen;
+    private BooleanProperty ivExtraHitcheck;
+    private StringProperty ivMissionBriefing;
+    private StringProperty ivArticleDefinitionFilepath;
+    private StringProperty ivBD1Filepath;
+    private StringProperty ivImage1Filepath;
+    private StringProperty ivImage2Filepath;
+    private StringProperty ivMissionLongName;
+    private StringProperty ivMissionShortName;
+    private StringProperty ivPD1Filepath;
+    private IntegerProperty ivSkyType;
+    private IntegerProperty mMaxNumHalfWidthCharactersInLine;
+    private IntegerProperty mMaxNumLines;
+    private StringProperty mReadEncoding;
+    private StringProperty mWriteEncoding;
+    private StringProperty tCustomThemeFilepath;
+    private IntegerProperty wWindowHeight;
+    private IntegerProperty wWindowWidth;
+
+    private List<DisplayLanguage> cbLDisplayLanguageItems;
+    private List<MiffieTheme> cbTThemeItems;
+
+    public PreferencesViewModel() {
+        lDisplayLanguage = new SimpleObjectProperty<>();
+        tTheme = new SimpleObjectProperty<>();
+        ivDarkScreen = new SimpleBooleanProperty();
+        ivExtraHitcheck = new SimpleBooleanProperty();
+        ivMissionBriefing = new SimpleStringProperty();
+        ivArticleDefinitionFilepath = new SimpleStringProperty();
+        ivBD1Filepath = new SimpleStringProperty();
+        ivImage1Filepath = new SimpleStringProperty();
+        ivImage2Filepath = new SimpleStringProperty();
+        ivMissionLongName = new SimpleStringProperty();
+        ivMissionShortName = new SimpleStringProperty();
+        ivPD1Filepath = new SimpleStringProperty();
+        ivSkyType = new SimpleIntegerProperty();
+        mMaxNumHalfWidthCharactersInLine = new SimpleIntegerProperty();
+        mMaxNumLines = new SimpleIntegerProperty();
+        mReadEncoding = new SimpleStringProperty();
+        mWriteEncoding = new SimpleStringProperty();
+        tCustomThemeFilepath = new SimpleStringProperty();
+        wWindowHeight = new SimpleIntegerProperty();
+        wWindowWidth = new SimpleIntegerProperty();
+    }
+
+    /**
+     * Populate the view.
+     * Call this method after binding is done.
+     *
+     * @param cbLDisplayLanguage Combobox for display language
+     * @param cbTTheme           Combobox for window theme
+     * @return {@code true} if success, {@code false} if error
+     */
+    public boolean populate(
+            ComboBox<DisplayLanguage> cbLDisplayLanguage,
+            ComboBox<MiffieTheme> cbTTheme) {
+        if (DisplayLanguages.get().isEmpty()) {
+            logger.error("Cannot populate the preferences view because language list is empty");
+            return true;
+        }
+        if (MiffieThemes.get().isEmpty()) {
+            logger.error("Cannot populate the preferences view because theme list is empty");
+            return true;
+        }
+
+        cbLDisplayLanguageItems = DisplayLanguages.get().get().getSortedList();
+        cbLDisplayLanguage.getItems().addAll(cbLDisplayLanguageItems);
+        cbLDisplayLanguage.setValue(cbLDisplayLanguageItems.get(0));
+        Callback<ListView<DisplayLanguage>, ListCell<DisplayLanguage>> displayLanguageFactory
+                = lv -> new ListCell<>() {
+            @Override
+            protected void updateItem(DisplayLanguage item, boolean empty) {
+                super.updateItem(item, empty);
+
+                if (empty) {
+                    setText("");
+                } else {
+                    setText(item.toString());
+                }
+            }
+        };
+        cbLDisplayLanguage.setCellFactory(displayLanguageFactory);
+        cbLDisplayLanguage.setButtonCell(displayLanguageFactory.call(null));
+
+        cbTThemeItems = MiffieThemes.get().get().themes;
+        cbTTheme.getItems().addAll(cbTThemeItems);
+        cbTTheme.setValue(cbTThemeItems.get(0));
+        Callback<ListView<MiffieTheme>, ListCell<MiffieTheme>> themeFactory
+                = lv -> new ListCell<>() {
+            @Override
+            protected void updateItem(MiffieTheme item, boolean empty) {
+                super.updateItem(item, empty);
+
+                if (empty) {
+                    setText("");
+                } else {
+                    setText(item.displayName());
+                }
+            }
+        };
+        cbTTheme.setCellFactory(themeFactory);
+        cbTTheme.setButtonCell(themeFactory.call(null));
+
+        return false;
+    }
+
+    public boolean isMIFReadEncodingSupported() {
+        return Charset.isSupported(mReadEncoding.get());
+    }
+
+    public boolean isMIFWriteEncodingSupported() {
+        return Charset.isSupported(mWriteEncoding.get());
+    }
+
+    public SingleSelectionModel<DisplayLanguage> getlDisplayLanguage() {
+        return lDisplayLanguage.get();
+    }
+
+    public ObjectProperty<SingleSelectionModel<DisplayLanguage>> lDisplayLanguageProperty() {
+        return lDisplayLanguage;
+    }
+
+    public void setlDisplayLanguage(SingleSelectionModel<DisplayLanguage> lDisplayLanguage) {
+        this.lDisplayLanguage.set(lDisplayLanguage);
+    }
+
+    public SingleSelectionModel<MiffieTheme> gettTheme() {
+        return tTheme.get();
+    }
+
+    public ObjectProperty<SingleSelectionModel<MiffieTheme>> tThemeProperty() {
+        return tTheme;
+    }
+
+    public void settTheme(SingleSelectionModel<MiffieTheme> tTheme) {
+        this.tTheme.set(tTheme);
+    }
+
+    public boolean isIvDarkScreen() {
+        return ivDarkScreen.get();
+    }
+
+    public BooleanProperty ivDarkScreenProperty() {
+        return ivDarkScreen;
+    }
+
+    public void setIvDarkScreen(boolean ivDarkScreen) {
+        this.ivDarkScreen.set(ivDarkScreen);
+    }
+
+    public boolean isIvExtraHitcheck() {
+        return ivExtraHitcheck.get();
+    }
+
+    public BooleanProperty ivExtraHitcheckProperty() {
+        return ivExtraHitcheck;
+    }
+
+    public void setIvExtraHitcheck(boolean ivExtraHitcheck) {
+        this.ivExtraHitcheck.set(ivExtraHitcheck);
+    }
+
+    public String getIvMissionBriefing() {
+        return ivMissionBriefing.get();
+    }
+
+    public StringProperty ivMissionBriefingProperty() {
+        return ivMissionBriefing;
+    }
+
+    public void setIvMissionBriefing(String ivMissionBriefing) {
+        this.ivMissionBriefing.set(ivMissionBriefing);
+    }
+
+    public String getIvArticleDefinitionFilepath() {
+        return ivArticleDefinitionFilepath.get();
+    }
+
+    public StringProperty ivArticleDefinitionFilepathProperty() {
+        return ivArticleDefinitionFilepath;
+    }
+
+    public void setIvArticleDefinitionFilepath(String ivArticleDefinitionFilepath) {
+        this.ivArticleDefinitionFilepath.set(ivArticleDefinitionFilepath);
+    }
+
+    public String getIvBD1Filepath() {
+        return ivBD1Filepath.get();
+    }
+
+    public StringProperty ivBD1FilepathProperty() {
+        return ivBD1Filepath;
+    }
+
+    public void setIvBD1Filepath(String ivBD1Filepath) {
+        this.ivBD1Filepath.set(ivBD1Filepath);
+    }
+
+    public String getIvImage1Filepath() {
+        return ivImage1Filepath.get();
+    }
+
+    public StringProperty ivImage1FilepathProperty() {
+        return ivImage1Filepath;
+    }
+
+    public void setIvImage1Filepath(String ivImage1Filepath) {
+        this.ivImage1Filepath.set(ivImage1Filepath);
+    }
+
+    public String getIvImage2Filepath() {
+        return ivImage2Filepath.get();
+    }
+
+    public StringProperty ivImage2FilepathProperty() {
+        return ivImage2Filepath;
+    }
+
+    public void setIvImage2Filepath(String ivImage2Filepath) {
+        this.ivImage2Filepath.set(ivImage2Filepath);
+    }
+
+    public String getIvMissionLongName() {
+        return ivMissionLongName.get();
+    }
+
+    public StringProperty ivMissionLongNameProperty() {
+        return ivMissionLongName;
+    }
+
+    public void setIvMissionLongName(String ivMissionLongName) {
+        this.ivMissionLongName.set(ivMissionLongName);
+    }
+
+    public String getIvMissionShortName() {
+        return ivMissionShortName.get();
+    }
+
+    public StringProperty ivMissionShortNameProperty() {
+        return ivMissionShortName;
+    }
+
+    public void setIvMissionShortName(String ivMissionShortName) {
+        this.ivMissionShortName.set(ivMissionShortName);
+    }
+
+    public String getIvPD1Filepath() {
+        return ivPD1Filepath.get();
+    }
+
+    public StringProperty ivPD1FilepathProperty() {
+        return ivPD1Filepath;
+    }
+
+    public void setIvPD1Filepath(String ivPD1Filepath) {
+        this.ivPD1Filepath.set(ivPD1Filepath);
+    }
+
+    public int getIvSkyType() {
+        return ivSkyType.get();
+    }
+
+    public IntegerProperty ivSkyTypeProperty() {
+        return ivSkyType;
+    }
+
+    public void setIvSkyType(int ivSkyType) {
+        this.ivSkyType.set(ivSkyType);
+    }
+
+    public int getmMaxNumHalfWidthCharactersInLine() {
+        return mMaxNumHalfWidthCharactersInLine.get();
+    }
+
+    public IntegerProperty mMaxNumHalfWidthCharactersInLineProperty() {
+        return mMaxNumHalfWidthCharactersInLine;
+    }
+
+    public void setmMaxNumHalfWidthCharactersInLine(int mMaxNumHalfWidthCharactersInLine) {
+        this.mMaxNumHalfWidthCharactersInLine.set(mMaxNumHalfWidthCharactersInLine);
+    }
+
+    public int getmMaxNumLines() {
+        return mMaxNumLines.get();
+    }
+
+    public IntegerProperty mMaxNumLinesProperty() {
+        return mMaxNumLines;
+    }
+
+    public void setmMaxNumLines(int mMaxNumLines) {
+        this.mMaxNumLines.set(mMaxNumLines);
+    }
+
+    public String getmReadEncoding() {
+        return mReadEncoding.get();
+    }
+
+    public StringProperty mReadEncodingProperty() {
+        return mReadEncoding;
+    }
+
+    public void setmReadEncoding(String mReadEncoding) {
+        this.mReadEncoding.set(mReadEncoding);
+    }
+
+    public String getmWriteEncoding() {
+        return mWriteEncoding.get();
+    }
+
+    public StringProperty mWriteEncodingProperty() {
+        return mWriteEncoding;
+    }
+
+    public void setmWriteEncoding(String mWriteEncoding) {
+        this.mWriteEncoding.set(mWriteEncoding);
+    }
+
+    public String gettCustomThemeFilepath() {
+        return tCustomThemeFilepath.get();
+    }
+
+    public StringProperty tCustomThemeFilepathProperty() {
+        return tCustomThemeFilepath;
+    }
+
+    public void settCustomThemeFilepath(String tCustomThemeFilepath) {
+        this.tCustomThemeFilepath.set(tCustomThemeFilepath);
+    }
+
+    public int getwWindowHeight() {
+        return wWindowHeight.get();
+    }
+
+    public IntegerProperty wWindowHeightProperty() {
+        return wWindowHeight;
+    }
+
+    public void setwWindowHeight(int wWindowHeight) {
+        this.wWindowHeight.set(wWindowHeight);
+    }
+
+    public int getwWindowWidth() {
+        return wWindowWidth.get();
+    }
+
+    public IntegerProperty wWindowWidthProperty() {
+        return wWindowWidth;
+    }
+
+    public void setwWindowWidth(int wWindowWidth) {
+        this.wWindowWidth.set(wWindowWidth);
+    }
+}
