f57d7c7e63e232b64668113bc9d293e9245fda4f
maeda6uiui <63230315+maeda6uiui@users.noreply.github.com>
2023-11-02 16:11:56 +0900

    Change to public

--- src/main/java/com/github/maeda6uiui/miffie/MiffieSettings.java
+++ src/main/java/com/github/maeda6uiui/miffie/MiffieSettings.java
@@ -185,7 +185,7 @@ public class MiffieSettings {
 
     private static MiffieSettings instance;
 
-    private MiffieSettings() {
+    public MiffieSettings() {
         languageSettings = new LanguageSettings();
         windowSettings = new WindowSettings();
         themeSettings = new ThemeSettings();
