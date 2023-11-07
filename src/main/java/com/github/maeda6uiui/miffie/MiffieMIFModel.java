b099ffa48adbbd6aaa04f41c202e93b4733148ca
maeda6uiui <63230315+maeda6uiui@users.noreply.github.com>
2023-11-07 09:30:09 +0900

    Add methods to save MIF as JSON and YAML

--- src/main/java/com/github/maeda6uiui/miffie/MiffieMIFModel.java
+++ src/main/java/com/github/maeda6uiui/miffie/MiffieMIFModel.java
@@ -1,10 +1,13 @@
 package com.github.maeda6uiui.miffie;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 import com.github.dabasan.jxm.mif.MIFManipulator;
 import com.github.dabasan.jxm.mif.MissionInfo;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
 
 /**
  * Model to handle MIF data
@@ -21,5 +24,17 @@ public class MiffieMIFModel {
         var manipulator = new MIFManipulator();
         manipulator.setMissionInfo(missionInfo);
         manipulator.saveAsMIF(file, encoding);
+    }
+
+    public void saveMIFAsJSON(MissionInfo missionInfo, File file) throws IOException {
+        var mapper = new ObjectMapper();
+        String json = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(missionInfo);
+        Files.writeString(file.toPath(), json);
+    }
+
+    public void saveMIFAsYAML(MissionInfo missionInfo, File file) throws IOException {
+        var mapper = new ObjectMapper(new YAMLFactory());
+        String yaml = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(missionInfo);
+        Files.writeString(file.toPath(), yaml);
     }
 }
