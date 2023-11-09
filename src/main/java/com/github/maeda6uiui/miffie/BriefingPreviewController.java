d45d2e236fcd92e90f80458c8a21a60b23e41dc9
maeda6uiui <63230315+maeda6uiui@users.noreply.github.com>
2023-11-09 14:00:28 +0900

    Add BriefingPreviewController.java

---
+++ src/main/java/com/github/maeda6uiui/miffie/BriefingPreviewController.java
@@ -0,0 +1,23 @@
+package com.github.maeda6uiui.miffie;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TextArea;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+/**
+ * Controller for the briefing preview view
+ *
+ * @author maeda6uiui
+ */
+public class BriefingPreviewController implements Initializable {
+    @FXML
+    private TextArea taBriefingPreview;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
