cc3acb4d6b29fccb3642c30982702aa3dce5f085
maeda6uiui <63230315+maeda6uiui@users.noreply.github.com>
2023-11-13 18:09:35 +0900

    Add required modules

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,4 +1,5 @@
 module com.github.maeda6uiui.miffie {
+    requires java.naming;
     requires javafx.controls;
     requires javafx.fxml;
     requires atlantafx.base;
@@ -6,6 +7,7 @@ module com.github.maeda6uiui.miffie {
     requires com.fasterxml.jackson.databind;
     requires com.fasterxml.jackson.dataformat.yaml;
     requires org.slf4j;
+    requires ch.qos.logback.classic;
     requires com.github.dabasan.jxm.mif;
 
     opens com.github.maeda6uiui.miffie to javafx.fxml, com.fasterxml.jackson.databind;
